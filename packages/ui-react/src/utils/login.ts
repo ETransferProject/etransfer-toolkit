477a355902b047ce61b676bb25d923d49a50625b
doraemon <doraemon@etransfer.exchange>
2024-08-15 19:50:31 +0800

    fix: 🐛 add reCaptcha function params - networkType

--- packages/ui-react/src/utils/login.ts
+++ packages/ui-react/src/utils/login.ts
@@ -1,15 +1,26 @@
 import googleReCaptchaModal from '../components/GoogleReCaptcha/googleReCaptchaModal';
 import { ReCaptchaType } from '../components/GoogleReCaptcha/types';
+import { ETransferConfig } from '../provider/ETransferConfigProvider';
+import { NetworkType } from '../types';
 import { etransferCore } from './core';
 
-export async function getETransferReCaptcha(walletAddress: string): Promise<string | undefined> {
+export async function getETransferReCaptcha(
+  walletAddress: string,
+  networkType?: NetworkType,
+  modalWidth?: number,
+): Promise<string | undefined> {
+  const networkTypeNew = networkType || getNetworkType();
   const isRegistered = await etransferCore.services.checkEOARegistration({ address: walletAddress });
   if (!isRegistered.result) {
-    const result = await googleReCaptchaModal();
+    const result = await googleReCaptchaModal(networkTypeNew, modalWidth);
     if (result.type === ReCaptchaType.success) {
       return result.data;
     }
   }
 
   return undefined;
+}
+
+export function getNetworkType() {
+  return ETransferConfig.getConfig('networkType') as NetworkType;
 }
